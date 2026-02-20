89a26e8cb7d4035349e6eaa6f8989f2ff8c5450c
wad218 <wad.2015.98@gmail.com>
2026-02-20 15:46:47 +0200

    Update trak2.js

--- trak2.js
+++ trak2.js
@@ -1,7 +1,6 @@
 (function () {
     'use strict';
     Lampa.Utils.putScriptAsync(
-        ['https://wad218.github.io/lmptrk/traks2.js'],
-        function () { }
+        ['https://wad218.github.io/lmptrk/traks2.js'], function () { }
     );
 })();
