551ad1f07dde512c04454f90b89d4a038b43ac57
wad218 <wad.2015.98@gmail.com>
2026-03-28 20:16:03 +0200

    Update tracks.js

--- tracks.js
+++ tracks.js
@@ -2,45 +2,56 @@
     'use strict';
 
     var omdbApiKey = '7d0a0115';
-    var ratingCache = {};
+    var seasonCache = {};
 
-    function getRating(imdbId, title, season, episode, callback) {
-        var cacheKey = (imdbId || title) + '_S' + season + '_E' + episode;
-        if (ratingCache[cacheKey]) return callback(ratingCache[cacheKey]);
+    function getSeasonData(imdbId, title, season, callback) {
+        var cacheKey = (imdbId || title) + '_S' + season;
+        if (seasonCache[cacheKey]) return callback(seasonCache[cacheKey]);
 
+        // Пріоритет на ID, як у вашому успішному посиланні
         var query = imdbId ? 'i=' + imdbId : 't=' + encodeURIComponent(title);
-        var url = 'https://www.omdbapi.com/?apikey=' + omdbApiKey + '&' + query + '&Season=' + season + '&Episode=' + episode;
+        var url = 'https://www.omdbapi.com/?apikey=' + omdbApiKey + '&' + query + '&Season=' + season;
         
         $.getJSON(url, function(data) {
-            var r = (data && data.imdbRating && data.imdbRating !== 'N/A') ? data.imdbRating : 'N/A';
-            ratingCache[cacheKey] = r;
-            callback(r);
+            var ratings = {};
+            if (data && data.Episodes) {
+                data.Episodes.forEach(function(ep) {
+                    var epNum = parseInt(ep.Episode);
+                    if (!isNaN(epNum)) {
+                        ratings[epNum] = ep.imdbRating;
+                    }
+                });
+            }
+            seasonCache[cacheKey] = ratings;
+            callback(ratings);
         });
     }
 
     function init() {
-        // Слухаємо подію фокусу на елементі списку
         Lampa.Listener.follow('torrent_file', function (e) {
+            // Реагуємо на виділення (hover) серії у списку
             if (e.type == 'hover' && e.item) {
                 var item = e.item;
                 var card = Lampa.Activity.active().card || {};
                 var imdbId = card.imdb_id || '';
                 var title = card.title || card.name || '';
                 
+                // Якщо це серіал (є сезон та епізод)
                 if (item.season && item.episode && (imdbId || title)) {
-                    // Очищаємо заголовок (якщо немає ID)
-                    if (!imdbId) title = title.split(/[(\[sS\d{1,2}[eE]/)[0].trim();
-
-                    getRating(imdbId, title, item.season, item.episode, function(rating) {
-                        // Знаходимо або створюємо панель для рейтингу
+                    getSeasonData(imdbId, title, item.season, function(ratings) {
+                        var rating = ratings[parseInt(item.episode)] || 'N/A';
+                        
+                        // Шукаємо блок з тех. інфою (куди писав минулий плагін)
                         var container = $('.tracks-metainfo');
                         if (container.length) {
-                            var imdbBlock = container.find('.imdb-panel-rating');
+                            var imdbBlock = container.find('.imdb-live-rating');
                             if (!imdbBlock.length) {
-                                imdbBlock = $('<div class="imdb-panel-rating" style="margin-top: 5px; color: #f5c518; font-weight: bold; font-size: 1.2em; border-top: 1px solid rgba(255,255,255,0.1); padding-top: 5px;"></div>');
+                                imdbBlock = $('<div class="imdb-live-rating" style="margin-top: 8px; padding-top: 8px; border-top: 1px solid rgba(255,255,255,0.1); color: #f5c518; font-weight: bold; font-size: 1.3em;"></div>');
                                 container.append(imdbBlock);
                             }
-                            imdbBlock.html('★ IMDb: ' + rating + ' <span style="font-size: 0.7em; color: #aaa; margin-left: 10px;">(S' + item.season + ' E' + item.episode + ')</span>');
+                            
+                            var starColor = rating !== 'N/A' ? '#f5c518' : '#666';
+                            imdbBlock.html('<span style="color:' + starColor + '">★ IMDb: ' + rating + '</span> <span style="font-size: 0.6em; color: #aaa; margin-left: 10px; font-weight: normal;">Episode ' + item.episode + '</span>');
                         }
                     });
                 }
@@ -50,19 +61,19 @@
 
     var manifest = {
         type: "other",
-        version: "2.0.0",
-        name: "IMDb Bottom Panel",
-        description: "Рейтинг серії в нижній панелі інформації",
-        component: "imdb_panel"
+        version: "2.1.0",
+        name: "IMDb Smart Panel",
+        description: "Виводить рейтинг серії з масиву сезону",
+        component: "imdb_smart"
     };
 
     function start() {
-        window.imdb_panel_ready = true;
+        window.imdb_smart_ready = true;
         Lampa.Manifest.plugins = manifest;
         init();
     }
 
-    if (!window.imdb_panel_ready) {
+    if (!window.imdb_smart_ready) {
         if (window.appready) start();
         else Lampa.Listener.follow("app", function (e) { if (e.type === "ready") start(); });
     }
